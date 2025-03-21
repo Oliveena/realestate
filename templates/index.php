af8768d71abfd950b8be8355da4b9d8b92d66c82
Ana Tara <ana.tara@outlook.com>
2025-03-21 09:31:15 -0400

    updated daily scrum, played around with Monolog

--- templates/index.php
+++ templates/index.php
@@ -1,1 +1,12 @@
-<?php ?>
+<!--
+
+session_start();
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('main');
+$log->pushHandler(new StreamHandler(dirname(__FILE__) . '/applogs/everything.log', Logger::DEBUG));
+$log->pushHandler(new StreamHandler(dirname(__FILE__) . '/applogs/errors.log', Logger::ERROR));
+
+from day 14-->
