6633cdc2862d90d4e9d388f3f41503fe2a428190
Ana Tara <ana.tara@outlook.com>
2025-03-23 10:09:28 -0400

    updated daily scrum + working on Google Maps API

--- templates/index.php
+++ templates/index.php
@@ -9,4 +9,4 @@ $log = new Logger('main');
 $log->pushHandler(new StreamHandler(dirname(__FILE__) . '/applogs/everything.log', Logger::DEBUG));
 $log->pushHandler(new StreamHandler(dirname(__FILE__) . '/applogs/errors.log', Logger::ERROR));
 
-from day 14-->
+from day 14-->+
