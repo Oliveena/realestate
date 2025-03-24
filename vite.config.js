4ed41f1a22de6ef81c3e6594f867dff35081a184
trangtrieu-jac <6341464@johnabbottcollege.net>
2025-03-24 18:38:08 -0400

    update laravel+frontend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// import tailwindcss from '@tailwindcss/vite';
-import * as bootstrap from 'bootstrap';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -9,6 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        //tailwindcss(),
+        tailwindcss(),
     ],
 });
